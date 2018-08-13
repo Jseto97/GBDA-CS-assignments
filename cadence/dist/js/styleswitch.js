f10316e2e7a69f9b627ffc256f80fc5136bd148b
Jseto97 <jsetomsc@gmail.com>
2018-08-13 15:12:45 -0400

    labs + assisngments cs 105

--- cadence/dist/js/styleswitch.js
+++
@@ -1,6 +0,0 @@
-function chooseStyle(styletitle, days) { //Interface function to switch style sheets plus save "title" attr of selected stylesheet to cookie
-if (document.getElementById){
-setStylesheet(styletitle)
-setCookie("mysheet", styletitle, days)
-}
-}
